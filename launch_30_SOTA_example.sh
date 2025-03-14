40c486452ca386bf1405ddef182ac3cd0b2f8095
martins.kuznecovs <martins.kuznecovs@gmail.com>
2025-03-14 15:41:04 +0200

    example launch script

---
+++ launch_30_SOTA_example.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+#SBATCH --account project_465001281
+#SBATCH --partition standard-g
+#SBATCH --exclusive=user
+#SBATCH --nodes=3
+#SBATCH --gpus-per-node=mi250:8
+#SBATCH --tasks-per-node=8
+#SBATCH --cpus-per-task=7
+#SBATCH --mem=0
+#SBATCH --time=48:00:00
+#SBATCH --hint=nomultithread
+#SBATCH --job-name=fixed_test
+#Comments:
+#  partition - dev-g when debugging on up to 32 nodes. standard-g when working with more nodes.
+#  exclusive=user - Slurm will alocate us full nodes.
+#  gpus-per-node=mi250:8 - use all GPUs on the nodes.
+#  tasks-per-node=8 - will launch one process per GPU.
+#  cpus-per-task - 7 is the maximum that doesn't crash.
+#  mem=0 - unlimited RAM. 512GB RAM per node.
+#  hint=nomultithread - make it so that no cpu has more than one process running on it at the same time.
+
+set -euo pipefail
+
+NEOX_DIR="/project/project_465001281/IP/llm-gpt-neox"
+
+export CC=gcc-12
+export CXX=g++-12
+
+#Don't understand, maybe necessary.
+export MEMORY_OPT_ALLREDUCE_SIZE=100000000
+
+#Don't understand, but necessary.
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+#Setting up torch distributed env parameters.
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_PORT=9835
+export WORLD_SIZE=$SLURM_NTASKS
+
+#Don't fully understand.
+#Necessary for faster internode communication.
+export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
+
+#Don't understand. Necessary for faster internode communication, I think.
+export OMP_NUM_THREADS=1
+
+#Don't understand. Necessary for faster memory access between nodes, I think.
+export NCCL_NET_GDR_LEVEL=PHB
+
+#A variety of debug output flags.
+#Hasn't really helped me much.
+#export NCCL_DEBUG=INFO
+#export RCCL_KERNEL_COLL_TRACE_ENABLE=1
+#export NCCL_DEBUG_SUBSYS=INIT,COLL
+
+#Activate a virtual environment for launching gpt-neox's slurm launcher.
+#Load some lmod modules. Not sure if all were necessary.
+module purge
+module load CrayEnv
+module load cray-python/3.9.13.1
+#module load rocm/6.2.2
+module load gcc/12.2.0
+source /scratch/project_465001281/containers/launch_conda_env.sh
+
+#Tell singularity to bind /project /scratch /flash folders etc.
+module use /appl/local/training/modules/AI-20240529/
+module load singularity-userfilesystems
+
+#Not sure if this is necessary.
+#I think this was copied over from one of SILO.AIs configs.
+mkdir -p workdir
+wd=$(realpath workdir)
+if [ ! -d "$wd"/cray-deps ] ; then
+  rm -rf "$wd"/cray-deps
+  mkdir "$wd"/cray-deps
+  cp /usr/lib64/libcxi* $wd/cray-deps
+fi
+
+#Generating a file so deepy.py knows the available resources.
+#(deepy.py is the gpt-neox launcher script.)
+GPUS_PER_NODE=8
+mkdir -p ./hostfiles
+hostfile=./hostfiles/hosts_$SLURM_JOBID
+# loop over the node names
+for i in `scontrol show hostnames $SLURM_NODELIST`
+do
+    # add a line to the hostfile
+    echo $i slots=$GPUS_PER_NODE >>$hostfile
+done
+export DLTS_HOSTFILE=./hostfiles/hosts_$SLURM_JOBID
+
+#This command will tell deepy.py to run training with the config 00_example.yml.
+CMD="$NEOX_DIR/deepy.py \
+  $NEOX_DIR/train.py \
+  $NEOX_DIR/launch_scripts/full_pipe_test_merged_fix_nobias_refix/N_2/30_SOTA_962M_N.yml
+  "
+
+
+$CMD
