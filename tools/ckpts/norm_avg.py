bbc586f437930541c57a9b03595de92b202baa88
Ingus <ingus.pretkalnins@tilde.lv>
2025-04-10 16:40:19 +0300

    Add script that averages checkpoint layernorms along TP ranks

---
+++ tools/ckpts/norm_avg.py
@@ -0,0 +1,95 @@
+import argparse
+import torch
+import os
+import re
+
+def parse_args():
+  parser = argparse.ArgumentParser(
+    description="Creates new GPT-NeoX checkpoints with averaged RMSNorm scale/weight tensors between TP. " +
+                "Doesn't work for all possible neox configs. But works for our 30B model. " +
+                "Also note the code is hardcoded for bf16 checkpoints."
+  )
+  
+  parser.add_argument(
+    "--input-folder",
+    type=str,
+    required=True,
+    help="Path to checkpoint folder (e.g. /a/b/c/global_step1337)."
+  )
+  
+  parser.add_argument(
+    "--output-folder",
+    type=str,
+    default=None,
+    help="Optional path for output. If not provided will just overwrite the checkpoint files."
+  )
+  
+  return parser.parse_args()
+
+def zero_pad(num):
+  if num < 10:
+    return "0" + str(num)
+  else:
+    return str(num)
+
+def main():
+  args = parse_args()
+  
+  # Detect number of ranks.
+  print("Detecting rank count. :3")
+  highest_rank = -1
+  for file_name in os.listdir(args.input_folder):
+    match = re.match(r"^mp_rank_(\d+)_model_states\.pt$", file_name)
+    if match:
+      rank = int(match.group(1))
+      if rank > highest_rank:
+        highest_rank = rank
+        highest_file = file_name
+  rank_count = highest_rank + 1
+  
+  if highest_rank == -1:
+    raise Exception("Didn't find any mp_rank_XX_model_states.pt file in the --input-folder!")
+  
+  print("Detected " + str(rank_count) + " ranks. :3")
+  
+
+  # Load checkpoint ranks.
+  print("Loading ranks. :3")
+  ranks = []
+  for rank_idx in range(rank_count):
+    rank_path = os.path.join(args.input_folder, "mp_rank_" + zero_pad(rank_idx) + "_model_states.pt")
+    ranks.append(torch.load(rank_path, map_location="cpu"))
+  
+  # Average layernorms.
+  print("Averaging layernorms. :3")
+  for tensor_name in ranks[0]["module"]:
+    if not ("norm.scale" in tensor_name):
+      # Not an rmsnorm scale vector, skipping.
+      continue
+    
+    # Calc mean.
+    total = ranks[0]["module"][tensor_name].to(torch.float32)
+    for rank_idx in range(1, rank_count):
+      total+= ranks[rank_idx]["module"][tensor_name].to(torch.float32)
+      avg = (total / rank_count).to(torch.bfloat16)
+    
+    # Overwrite previous values.
+    for rank_idx in range(0, rank_count):
+      ranks[rank_idx]["module"][tensor_name] = avg
+  
+  # Save.
+  print("Saving new ranks. :3")
+  if args.output_folder is not None:
+    output_folder = args.output_folder
+    os.makedirs(args.output_folder, exist_ok=True)
+  else:
+    print("Overwriting old ranks. Because you didn't provide the --output-folder ^_^''")
+    output_folder = args.input_folder
+  
+  for rank_idx in range(rank_count):
+    output_rank_path = os.path.join(output_folder, "mp_rank_" + zero_pad(rank_idx) + "_model_states.pt")
+    torch.save(ranks[rank_idx], output_rank_path)
+  print("Done! :d")    
+
+if __name__ == "__main__":
+  main()
