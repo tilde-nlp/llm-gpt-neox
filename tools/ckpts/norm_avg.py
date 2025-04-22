7f0dfc9000a2535e14fbad68a2f98e333cb0cb84
Ingus <ingus.pretkalnins@tilde.lv>
2025-04-22 11:35:09 +0300

    Add flushing to prints in layernorm averaging script

--- tools/ckpts/norm_avg.py
+++ tools/ckpts/norm_avg.py
@@ -2,6 +2,7 @@ import argparse
 import torch
 import os
 import re
+import shutil
 
 def parse_args():
   parser = argparse.ArgumentParser(
@@ -36,7 +37,7 @@ def main():
   args = parse_args()
   
   # Detect number of ranks.
-  print("Detecting rank count. :3")
+  print("Detecting rank count. :3", flush=True)
   highest_rank = -1
   for file_name in os.listdir(args.input_folder):
     match = re.match(r"^mp_rank_(\d+)_model_states\.pt$", file_name)
@@ -50,18 +51,18 @@ def main():
   if highest_rank == -1:
     raise Exception("Didn't find any mp_rank_XX_model_states.pt file in the --input-folder!")
   
-  print("Detected " + str(rank_count) + " ranks. :3")
+  print("Detected " + str(rank_count) + " ranks. :3", flush=True)
   
 
   # Load checkpoint ranks.
-  print("Loading ranks. :3")
+  print("Loading ranks. :3", flush=True)
   ranks = []
   for rank_idx in range(rank_count):
     rank_path = os.path.join(args.input_folder, "mp_rank_" + zero_pad(rank_idx) + "_model_states.pt")
     ranks.append(torch.load(rank_path, map_location="cpu"))
   
   # Average layernorms.
-  print("Averaging layernorms. :3")
+  print("Averaging layernorms. :3", flush=True)
   for tensor_name in ranks[0]["module"]:
     if not ("norm.scale" in tensor_name):
       # Not an rmsnorm scale vector, skipping.
@@ -78,18 +79,37 @@ def main():
       ranks[rank_idx]["module"][tensor_name] = avg
   
   # Save.
-  print("Saving new ranks. :3")
+  print("Saving new ranks. :3", flush=True)
   if args.output_folder is not None:
     output_folder = args.output_folder
     os.makedirs(args.output_folder, exist_ok=True)
   else:
-    print("Overwriting old ranks. Because you didn't provide the --output-folder ^_^''")
+    print("Overwriting old ranks. Because you didn't provide the --output-folder ^_^''", flush=True)
     output_folder = args.input_folder
-  
+
   for rank_idx in range(rank_count):
     output_rank_path = os.path.join(output_folder, "mp_rank_" + zero_pad(rank_idx) + "_model_states.pt")
     torch.save(ranks[rank_idx], output_rank_path)
-  print("Done! :d")    
+  
+  if args.output_folder is not None:
+    print("Oh also copying optimizer states. :3", flush=True)
+    pattern = re.compile(r"^mp_rank_(\d+)_model_states\.pt$")
+
+    for file_name in os.listdir(args.input_folder):
+      # Build full paths
+      source_path = os.path.join(args.input_folder, file_name)
+      destination_path = os.path.join(output_folder, file_name)
+
+      # Check if file matches our pattern
+      if pattern.match(file_name) is None:
+        # The file does not match, so we copy it
+        if os.path.isdir(source_path):
+          shutil.copytree(source_path, destination_path, dirs_exist_ok=True)
+        else:
+          shutil.copy2(source_path, destination_path)
+
+
+  print("Done! :d", flush=True)
 
 if __name__ == "__main__":
   main()
